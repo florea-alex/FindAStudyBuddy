b91e17253f6d70afffb7f4bff977865847888eb4
Vlad Toader <toadervlad01@gmail.com>
2022-06-13 15:58:43 +0300

    almost finished swipe carousel

--- src/components/SwipeCarousel/Main/data.js
+++ src/components/SwipeCarousel/Main/data.js
@@ -1,23 +1,32 @@
-export const pets = [
-    {
-      name: 'Cute Golden',
-      source: require('../../../assets/pet1.jpg'),
-    },
-    {
-      name: 'Curious Collie',
-      source: require('../../../assets/pet2.jpg'),
-    },
-    {
-      name: 'Nerd Dog',
-      source: require('../../../assets/pet3.jpg'),
-    },
-    {
-      name: 'Smiley Husky',
-      source: require('../../../assets/pet4.jpg'),
-    },
-    {
-      name: 'Serious Dog',
-      source: require('../../../assets/pet5.jpg'),
-    },
-  ];
-  
+import axios from 'axios';
+const usersId = localStorage.getItem("users").split(",");
+
+async function myfunc () {
+  const users = [];
+  console.log(usersId);
+  for(let i = 0; i<usersId.length; i++) {
+    var id = usersId[i];
+    var data1 = await axios.get("https://findastudybuddymds.azurewebsites.net/api/User/GetById?id="+id);
+    var prenume = data1.data.data.firstName;
+    var nume = data1.data.data.lastName;
+    var link3 = "https://findastudybuddymds.azurewebsites.net/api/Photos/Get-Image?userId="+id;
+    var photolink = "";
+    var data2 = await axios.get(link3)
+    photolink = data2.data.data;
+    if (photolink == "") {
+      photolink = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    }
+    const object = {
+      id: id,
+      name: prenume + " " + nume,
+      source: photolink
+    };
+    users.push(object);
+
+    
+}
+console.log(users);
+return users;
+}
+
+export const usersName = myfunc().then((users) => {return users});
